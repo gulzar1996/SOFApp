6f7a7a3489db75e1d51c9f6e12347ffc74c46dfe
Gulzar Ahmed <gulzarhome54@gmail.com>
2018-06-25 02:48:00 +0530

    Const class added

---
+++ app/src/main/java/github/gulzar1996/overflowsearch/Const.kt
@@ -0,0 +1,12 @@
+package github.gulzar1996.overflowsearch
+
+
+object Const {
+    const val BASE_URL = "https://api.stackexchange.com/2.2/"
+
+    const val DEFAULT_NETWORK_PAGE_SIZE = 45
+    const val INITIAL_PAGE = "1"
+    const val SORT = "activity"
+    const val ORDER = "desc"
+    const val SITE = "stackoverflow"
+}
