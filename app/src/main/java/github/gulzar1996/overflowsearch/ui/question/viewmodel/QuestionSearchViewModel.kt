332c3b441e79dda69008f62b9fd20b12110acd5d
Gulzar Ahmed <gulzarhome54@gmail.com>
2018-06-25 02:54:52 +0530

    Viewmodel + Factory (not possible to pass params)

--- app/src/main/java/github/gulzar1996/overflowsearch/ui/question/viewmodel/QuestionSearchViewModel.kt
+++ app/src/main/java/github/gulzar1996/overflowsearch/ui/question/viewmodel/QuestionSearchViewModel.kt
@@ -1,8 +1,35 @@
 package github.gulzar1996.overflowsearch.ui.question.viewmodel
 
+import android.arch.lifecycle.MutableLiveData
+import android.arch.lifecycle.Transformations
 import android.arch.lifecycle.ViewModel
-import github.gulzar1996.overflowsearch.data.local.question.QuestionSearchRepository
+import github.gulzar1996.overflowsearch.data.local.question.IQuestionSearchRepository
 
-class QuestionSearchViewModel(val questionSearchRepository: QuestionSearchRepository) : ViewModel() {
+class QuestionSearchViewModel(val repository: IQuestionSearchRepository) : ViewModel() {
+    private val questionQuery = MutableLiveData<String>()
+    private val repoResult = Transformations.map(questionQuery, {
+        repository.getQuestionsByQuery(it, 10)
+    })
+    val questions = Transformations.switchMap(repoResult, { it.pagedList })!!
+    val networkState = Transformations.switchMap(repoResult, { it.networkState })!!
+    val refreshState = Transformations.switchMap(repoResult, { it.refreshState })!!
 
+    fun refresh() {
+        repoResult.value?.refresh?.invoke()
+    }
+
+    fun showQuestions(questionQuery: String): Boolean {
+        if (this.questionQuery.value == questionQuery) {
+            return false
+        }
+        this.questionQuery.value = questionQuery
+        return true
+    }
+
+    fun retry() {
+        val listing = repoResult?.value
+        listing?.retry?.invoke()
+    }
+
+    fun currentQuery(): String? = questionQuery.value
 }
--- app/src/main/java/github/gulzar1996/overflowsearch/ui/question/viewmodel/QuestionSearchViewModelFactory.kt
+++ app/src/main/java/github/gulzar1996/overflowsearch/ui/question/viewmodel/QuestionSearchViewModelFactory.kt
@@ -2,10 +2,10 @@ package github.gulzar1996.overflowsearch.ui.question.viewmodel
 
 import android.arch.lifecycle.ViewModel
 import android.arch.lifecycle.ViewModelProvider
-import github.gulzar1996.overflowsearch.data.local.question.QuestionSearchRepository
+import github.gulzar1996.overflowsearch.data.local.question.IQuestionSearchRepository
 
 class QuestionSearchViewModelFactory
-(private val QuestionSearchRepository: QuestionSearchRepository) :
+(private val QuestionSearchRepository: IQuestionSearchRepository) :
         ViewModelProvider.Factory {
 
     override fun <T : ViewModel?> create(modelClass: Class<T>): T
