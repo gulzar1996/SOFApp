216552e5980bd2886462d9c59eba5314859d8e00
Gulzar Ahmed <gulzarhome54@gmail.com>
2018-06-25 02:44:51 +0530

    AppDatabse added

---
+++ app/src/main/java/github/gulzar1996/overflowsearch/data/local/AppDatabase.kt
@@ -0,0 +1,16 @@
+package github.gulzar1996.overflowsearch.data.local.question
+
+import android.arch.persistence.room.Database
+import android.arch.persistence.room.RoomDatabase
+import github.gulzar1996.overflowsearch.data.Question
+
+@Database(
+        entities = [(Question::class)],
+        version = 1,
+        exportSchema = false
+)
+abstract class AppDatabase : RoomDatabase() {
+
+    abstract fun question(): QuestionDao
+
+}
