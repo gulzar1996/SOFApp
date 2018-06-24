5592bbce7c66f96012cb770cace77aaef6a66cdf
Gulzar Ahmed <gulzarhome54@gmail.com>
2018-06-25 02:49:30 +0530

    Author data class added

---
+++ app/src/main/java/github/gulzar1996/overflowsearch/data/Author.kt
@@ -0,0 +1,13 @@
+package github.gulzar1996.overflowsearch.data
+
+import com.google.gson.annotations.SerializedName
+
+data class Author(
+        @SerializedName("reputation") val reputation: Int,
+        @SerializedName("user_id") val userId: Int,
+        @SerializedName("user_type") val userType: String,
+        @SerializedName("accept_rate") val acceptRate: Int,
+        @SerializedName("profile_image") val profileImage: String,
+        @SerializedName("display_name") val displayName: String,
+        @SerializedName("link") val link: String
+)
